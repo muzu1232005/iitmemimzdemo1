12877810c8449e703eed67127efaa6709baec3c5
Muzzammil Ahmed <muzu1232005@gmail.com>
2025-07-23 20:14:28 +0530

    added mzutils.py

---
+++ mzutils.py
@@ -0,0 +1,15 @@
+def area_of_circle(radius):
+    '''
+    Get  Area of circle for the given radius value.
+
+    area_of_circle(1) = 3.14
+    
+    '''
+    return 3.14 * radius
+
+
+
+# execute code when file is run directly
+# code inside this block wont execute when imported.
+if __name__ == "__main__":
+    print("Running area_of_circle for radius as 100 --> ",area_of_circle(100))
